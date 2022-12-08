c8a1ed300d6849be3e787c2830dfb9e573020f6a
AlbertoRoper <alberto.roperpol@colorado.edu>
2022-12-08 17:59:59 +0100

    Create GW_analytical.py

---
+++ GW_analytical.py
@@ -0,0 +1,12 @@
+"""
+GW_analytical.py is a Python routine that contains the functions to make
+calculations related to the analytical description of GW backgrounds produced
+by MHD turbulence in the early universe.
+It also includes some general mathematical functions that are useful.
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+import plot_sets
+import os
+
